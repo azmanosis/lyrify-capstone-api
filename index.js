b8e4a611b7e270193384c9d145efffc2378acb75
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-01-25 21:28:52 -0500

    installed helmet

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const SpotifyWebApi = require('spotify-web-api-node');
 const bodyParser = require("body-parser");
 const cors = require("cors");
+// Unsure if required
+// const helmet = require("helmet");
 
 // Import config
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+// Set Security Configs
+app.use(helmet());
 
 app.use(cors());
 app.use(bodyParser.json());
