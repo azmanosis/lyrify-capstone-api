559702b9efbf4829d73da9609dc002384871faaa
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-01-25 21:24:26 -0500

    Imp pkg: SptfyWbAp, bdyPrsr, crs, exprss, dtenv

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require("express");
-const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require("body-parser");
 const cors = require("cors");
+
+// Import config
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Server
+app.listen(PORT, () => {
+    console.log(`App running on Port #${PORT}`);
+});
