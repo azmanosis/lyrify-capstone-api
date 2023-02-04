e1123c12563c1fecbb2f560e9c837cca9ea1e165
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-04 00:52:12 -0500

    API Data set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const SpotifyWebApi = require('spotify-web-api-node');
+const lyricsFinder = require('lyrics-finder');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 // Unsure if required
@@ -16,6 +17,60 @@ app.use(helmet());
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    console.log("hi")
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    })
+
+    spotifyApi
+        .refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+            // console.log(data.body);
+        })
+        .catch((err) => {
+            // console.log(err)
+            res.sendStatus(400)
+        })
+})
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    })
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            })
+        })
+        .catch((err) => {
+            // console.log(err)
+            res.sendStatus(400)
+        })
+})
+
+app.get('/lyrics', async (req, res) => {
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || "No Lyrics Found"
+    res.json({ lyrics })
+})
 
 // Server
 app.listen(PORT, () => {
