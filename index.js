44ec0efa24f8fae3c4f882c59bccd5664f3104c0
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-02-14 22:13:56 -0500

    translation activated

--- index.js
+++ index.js
@@ -71,35 +71,35 @@ app.post('/login', (req, res) => {
 app.get('/lyrics', async (req, res) => {
     const lyrics = await lyricsFinder(req.query.artist, req.query.track) || "No Lyrics Found";
 
-    // const params = new url.URLSearchParams();
-    // params.append("from", "es");
-    // // params.append("from", "ar");
-    // // params.append("from", "ko");
-    // // params.append("from", "hi");
-    // params.append("to", "en");
-    // params.append("texts", `"español casa bonita"`);
+    const params = new url.URLSearchParams();
+    params.append("from", "es");
+    params.append("from", "ar");
+    params.append("from", "ko");
+    params.append("from", "hi");
+    params.append("to", "en");
+    params.append("texts", lyrics);
 
-    // let options = {
-    //     method: "POST",
-    //     url: "https://lecto-translation.p.rapidapi.com/v1/translate/text",
-    //     headers: {
-    //         "content-type": "application/x-www-form-urlencoded",
-    //         "x-rapidapi-host": "lecto-translation.p.rapidapi.com",
-    //         "x-rapidapi-key": process.env.TRANSLATEAPI_KEY,
-    //     },
-    //     data: params,
-    // };
+    let options = {
+        method: "POST",
+        url: "https://lecto-translation.p.rapidapi.com/v1/translate/text",
+        headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "x-rapidapi-host": "lecto-translation.p.rapidapi.com",
+            "x-rapidapi-key": process.env.TRANSLATEAPI_KEY,
+        },
+        data: params,
+    };
 
-    // axios.request(options)
-    //     .then(function (response) {
-    //         res.json({
-    //             lyrics, translation: response.data
-    //         });
-    //     })
-    //     .catch(function (error) {
-    //         console.error(error);
-    //         res.sendStatus(500);
-    //     });
+    axios.request(options)
+        .then(function (response) {
+            res.json({
+                lyrics, translation: response.data
+            });
+        })
+        .catch(function (error) {
+            console.error(error);
+            res.sendStatus(500);
+        });
 
     res.json({ lyrics })
 })
